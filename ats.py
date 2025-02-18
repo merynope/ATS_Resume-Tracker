d067e602313132fda28188f58849e20bb3493269
Meryn <114981662+merynope@users.noreply.github.com>
2025-02-18 17:32:02 +0530

    Update ats.py

--- ats.py
+++ ats.py
@@ -10,7 +10,7 @@ from PIL import Image
 import pdf2image
 import google.generativeai as genai
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("AIzaSyCuImlZkx-AbYoSrONVvfAr-vATsrQWnZE"))
 
 def gemini_response(input,pdf_content,prompt): 
     model=genai.GenerativeModel("gemini-1.5-pro")
